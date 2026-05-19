42af3a0eb67dd23f1e2fa1b54679b31cd5d65e86
Oren Benhaim <benhaimoren@gmail.com>
2026-05-19 03:40:17 +0300

    Use Vercel JavaScript API entrypoint

--- api/[...path].cjs
+++
@@ -1,7 +0,0 @@
-let appPromise;
-
-module.exports = async function handler(req, res) {
-  appPromise ||= import("../server.js").then((module) => module.default);
-  const app = await appPromise;
-  return app(req, res);
-};
